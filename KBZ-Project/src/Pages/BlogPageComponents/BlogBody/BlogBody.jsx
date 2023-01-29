ab23b3b7f349064ace428b6e5d7647512f3e6205
zaykha <zaykha@gmail.com>
2023-01-30 03:14:41 +0630

    blog update

--- KBZ-Project/src/Pages/BlogPageComponents/BlogBody/BlogBody.jsx
+++ KBZ-Project/src/Pages/BlogPageComponents/BlogBody/BlogBody.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { 
+    AuthorDetail,
+    AuthorName,
+    AuthorProfile,
     BlogBHeader, 
     BlogCard, 
     BlogCHeader, 
@@ -9,7 +12,28 @@ import {
     BlogDetails, 
     BlogImage, 
     BlogPhrase, 
-    BlogSidebar 
+    BlogSidebar, 
+    CardHeader, 
+    Catcontent, 
+    CatCount, 
+    Categories, 
+    Category, 
+    CatName, 
+    DatePosted,
+    RecentPost,
+    RpostDetail,
+    RPostDetailsDiv,
+    RPostdiv,
+    Rpostedtime,
+    RpostImg,
+    SCTA,
+    SearchBar,
+    SEmailInput,
+    Subscribediv,
+    SubscribeHeader,
+    Tag,
+    Tagdiv,
+    TagInflowdiv
 } from './BlogBodystyle'
 
 const BlogBody = () => {
@@ -20,8 +44,14 @@ const BlogBody = () => {
             <BlogContent>
                 <BlogCard>
                     <BlogImage></BlogImage>
-                    <BlogCHeader>hello</BlogCHeader>
-                    <BlogDetails>asdfaer</BlogDetails>
+                    <BlogCHeader>Major Crypo Exchanges Faces Action Over</BlogCHeader>
+                    <BlogDetails>
+                        <AuthorDetail>
+                            <AuthorProfile></AuthorProfile>
+                            <AuthorName>Darlene Robertson</AuthorName>
+                        </AuthorDetail>
+                        <DatePosted>5/27/15</DatePosted>
+                    </BlogDetails>
                     <BlogPhrase>asdfaefarwqefasvarveqrt qwert qrtqweq wer</BlogPhrase>
                     <BlogCTA>Read More</BlogCTA>
                 </BlogCard>
@@ -51,7 +81,78 @@ const BlogBody = () => {
                 </BlogCard>
 
             </BlogContent>
+
+
             <BlogSidebar>
+                <SearchBar type='text' placeholder='Search'/>
+
+                <Categories>
+                    <CardHeader>Categories</CardHeader>
+                    <Catcontent>
+                        <Category>
+                            <CatName>Alerts</CatName>
+                            <CatCount>35</CatCount>
+                        </Category>
+                        <Category>
+                            <CatName>Company News</CatName>
+                            <CatCount>28</CatCount>
+                        </Category>
+                    </Catcontent>
+                </Categories>
+
+                <RecentPost>
+                    <CardHeader>Recent Post</CardHeader>
+                    <RPostdiv>
+                        <RpostImg></RpostImg>
+                        <RPostDetailsDiv>
+                            <RpostDetail>Major Crypto Exchanges Face Action Over</RpostDetail>
+                            <Rpostedtime>8 hours ago</Rpostedtime>
+                        </RPostDetailsDiv>
+                    </RPostdiv>
+
+                    <RPostdiv>
+                        <RpostImg></RpostImg>
+                        <RPostDetailsDiv>
+                            <RpostDetail>Major Crypto Exchanges Face Action Over</RpostDetail>
+                            <Rpostedtime>8 hours ago</Rpostedtime>
+                        </RPostDetailsDiv>
+                    </RPostdiv>
+
+                    <RPostdiv>
+                        <RpostImg></RpostImg>
+                        <RPostDetailsDiv>
+                            <RpostDetail>Major Crypto Exchanges Face Action Over</RpostDetail>
+                            <Rpostedtime>8 hours ago</Rpostedtime>
+                        </RPostDetailsDiv>
+                    </RPostdiv>
+
+                    <RPostdiv>
+                        <RpostImg></RpostImg>
+                        <RPostDetailsDiv>
+                            <RpostDetail>Major Crypto Exchanges Face Action Over</RpostDetail>
+                            <Rpostedtime>8 hours ago</Rpostedtime>
+                        </RPostDetailsDiv>
+                    </RPostdiv>
+                </RecentPost>
+
+                <Tagdiv>
+                <CardHeader>Tag</CardHeader>
+                    <TagInflowdiv>
+                        <Tag>Money alerts</Tag>
+                        <Tag>News</Tag>
+                        <Tag>Technology</Tag>
+                        <Tag>crypto-currencies</Tag>
+                        <Tag>AML Outsourcing</Tag>
+                        <Tag>AML Services</Tag>
+                        <Tag>Support</Tag>
+                    </TagInflowdiv>
+                </Tagdiv>
+
+                <Subscribediv>
+                    <SubscribeHeader>Subscribe to KBZ Money Alerts</SubscribeHeader>
+                    <SEmailInput></SEmailInput>
+                    <SCTA>Subscribe Now</SCTA>
+                </Subscribediv>
 
             </BlogSidebar>
 
--- KBZ-Project/src/Pages/BlogPageComponents/BlogBody/BlogBodystyle.jsx
+++ KBZ-Project/src/Pages/BlogPageComponents/BlogBody/BlogBodystyle.jsx
@@ -7,56 +7,215 @@ export const BlogBHeader = styled.div`
     font-size: 2rem;
 `;
 export const BlogContentDiv = styled.div`
-    width: 60%;
+    width: 900px;
     height: auto;
-    border: 1px solid red;
     display: flex;
+    flex-wrap: wrap;
     margin: 40px auto;
 
 `;
 export const BlogContent = styled.div`
-    width: 70%;
+    width: 630px;
     height: auto;
-    border: 1px solid green;
-    margin:  10px auto;
+    margin:  0 auto;
     display: flex;
     flex-wrap: wrap;
 `;
 export const BlogSidebar = styled.div`
-    width: 18%;
+    width: 270px;
     height: auto;
-    border: 1px solid blue;
-    margin: 10px auto;
+    margin: 20px auto;
 `;
 export const BlogCard = styled.div`
-    width: 40%;
-    height: 400px;
-    border: 1px solid purple;
+    width: 280px;
+    // height: 400px;
+    // border: 1px solid purple;
     margin: 20px auto;
     border-radius: 10px;
+
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 `;
 export const BlogImage = styled.div`
-    width: 100%;
-    height: 30%;
-    border: 1px solid yellow;
+    width: 280px;
+    height: 250px;
+    background: rgba(0,225,225,0.7);
     margin: 0 auto 20px auto;
+    border-radius: 5px 5px 0 0;
 `;
 export const BlogCHeader = styled.div`
     width: 80%;
-    font-size: 1.3rem;
+    font-size: 1.1rem;
     margin: 20px auto;
 `;
 export const BlogDetails = styled.div`
     width: 80%;
     font-size: 0.7rem;
     margin: 20px auto;
+    display: flex;
+    justify-content: space-around;
 `;
 export const BlogPhrase = styled.div`
     width: 80%;
     margin: 20px auto;
+    font-size: 0.9rem;
+
 `;
 export const BlogCTA = styled.div`
     width: 80%;
     margin: 20px auto;
+    cursor:pointer;
+    font-size: 0.9rem;
+
 `;
+
+export const AuthorDetail = styled.div`
+`;
+export const AuthorProfile = styled.div``;
+export const AuthorName = styled.div``;
+export const DatePosted = styled.div``;
+
+export const SearchBar = styled.input`
+    width: 100%;
+    height: 30px;
+    outline: none;
+    border: 1px solid lightgrey;
+    background-color: white;
+    font-size: 1.1rem;
+    border-radius: 5px;
+`;
+export const Categories = styled.div`
+    width: 100%;
+    margin: 20px auto;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+export const CardHeader = styled.div`
+    // width: 100%;
+    margin: 0 0 10px 0;
+    padding: 10px 0 10px 20px;
+    font-size: 1rem;
+    font-weight: bold;
+    background-color:#F9FBFC ;
+    border-radius: 5px 5px 0 0;
+    // 
+    // border: 1px solid red;
+
+`;
+export const Catcontent = styled.div`
+    width: 80%;
+    margin: 20px auto;
+   
+`;
+export const Category = styled.div`
+    width: 100%;
+    display: flex;
+    margin: 10px auto;
+    justify-content: space-between;
+    
+`;
+export const CatName = styled.div`
+    font-size: 0.8rem;
+    
+`;
+export const CatCount = styled.div`
+    font-size: 0.8rem;    
+`;
+
+export const RecentPost = styled.div`
+    width: 100%;
+    margin: 20px auto;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    padding-bottom: 10px;
+`;
+export const RPostdiv = styled.div`
+    width: 80%;
+    margin: 10px auto;
+    display: flex;
+`;
+export const RpostImg = styled.div`
+    width: 100px;
+    height: 50px;
+    margin-right: 10px;
+    border: 1px solid green;
+    border-radius: 5px;
+`;
+export const RPostDetailsDiv = styled.div`
+    
+`;
+export const RpostDetail = styled.div`
+    font-size: 0.7rem;
+    line-height: 1.4;
+    font-weight: bold;
+`;
+export const Rpostedtime = styled.div`
+    font-size: 0.6rem;
+`;
+
+export const Tagdiv = styled.div` 
+    width: 100%;
+    margin: 20px auto;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+export const TagInflowdiv = styled.div`
+    width: 90%;
+    margin: 20px auto;
+    display: flex;
+    flex-wrap: wrap;
+
+    
+`;
+export const Tag = styled.div`
+    margin: 3px 3px;
+    font-size: 0.7rem;
+    background-color: F1F6F8;
+    padding: 0 8px;
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover{
+        background-color: grey;
+        color: white;
+    }
+`;
+export const Subscribediv = styled.div`
+    width: 100%;
+    margin: 20px auto;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    background-color: rgb(15,89,193);
+    color:white;
+    padding: 20px 0;
+`;
+export const SubscribeHeader = styled.div`
+    width: 80%;
+    margin: 20px auto;
+    text-align:center;
+    font-size: 1.5rem;
+`;
+export const SEmailInput = styled.div`
+    width: 80%;
+    height: 28px;
+    margin: 20px auto;
+    border: 1px solid grey;
+    border-radius: 5px;
+    background-color: white;
+`;
+export const SCTA = styled.div`
+    width: 120px;
+    padding: 4px 10px;
+    // border: 1px solid red;
+    text-align: center;
+    margin: auto;
+    border-radius: 5px;
+    background-color: #20C2FF;
+`;
+
+
+
+
 
